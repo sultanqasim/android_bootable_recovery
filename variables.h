b91578a005f81bec0e13276c47dbd53995f541d9
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-02-12 23:16:03 -0500

    Squid TWRP 3.0.0-r2

--- variables.h
+++ variables.h
@@ -17,7 +17,7 @@
 #ifndef _VARIABLES_HEADER_
 #define _VARIABLES_HEADER_
 
-#define TW_VERSION_STR              "3.4.5-r1"
+#define TW_VERSION_STR              "3.0.0-r2"
 
 #define TW_USE_COMPRESSION_VAR      "tw_use_compression"
 #define TW_FILENAME                 "tw_filename"
