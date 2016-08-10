a0a0ac9bf44b4c2520e2108bf10a56f0a2fbd33d
that <github@that.at>
2016-08-10 21:04:26 +0200

    Add git revision to startup message in recovery.log
    This should make it easier to find the exact source for
    a specific version of the binary.
    
    Change-Id: I7e04dd9a3205cc643150a588b69b4bd691212ecb

--- twrp.cpp
+++ twrp.cpp
@@ -112,7 +112,7 @@ int main(int argc, char **argv) {
 
 	time_t StartupTime = time(NULL);
 	ctime_no_newline = strtok(ctime(&StartupTime), "\n");
-	printf("Starting TWRP %s on %s (pid %d)\n", TW_VERSION_STR, ctime_no_newline, getpid());
+	printf("Starting TWRP %s-%s on %s (pid %d)\n", TW_VERSION_STR, TW_GIT_REVISION, ctime_no_newline, getpid());
 
 	// Load default values to set DataManager constants and handle ifdefs
 	DataManager::SetDefaultValues();
